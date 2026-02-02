e8650353c8a3928642b0a1e3fc1400ebf77db598
SellingJaguar <diogorcenteno@gmail.com>
2026-02-02 21:31:55 +0000

    Move monolith to init

--- meth/__init__.py
+++ meth/__init__.py
@@ -1,1 +1,15 @@
-# Make Python treat the directory as a package ☝️🤓
+"""
+No docstring provided, refuse to add one.
+"""
+
+# Constants
+
+PI = 3
+E = PI
+TAU = PI*2
+INF = 3000
+NAN = "Not a Number"
+
+# Meth exclusive constant
+
+TONY_STARK = "I love you <3 " * INF
