fce7db06baf831455963b9001122eeaa90cdbf21
SellingJaguar <diogorcenteno@gmail.com>
2026-02-03 01:50:45 +0000

    Add all function names

--- meth/__init__.py
+++ meth/__init__.py
@@ -2,6 +2,195 @@
 No docstring provided, refuse to add one.
 """
 
+# Number-theoretic functions
+
+def comb(n, k):
+    pass
+
+def factorial(n):
+    pass
+
+def gcd(*integers):
+    pass
+
+def isqrt(n):
+    pass
+
+def lcm(*integers):
+    pass
+
+def perm(n, k=None):
+    pass
+
+# Floating point arithmetic
+
+def ceil(x):
+    pass
+
+def fabs(x):
+    pass
+
+def floor(x):
+    pass
+
+def fma(x, y, z):
+    pass
+
+def fmod(x, y):
+    pass
+
+def modf(x):
+    pass
+
+def remainder(x, y):
+    pass
+
+def trunc(x):
+    pass
+
+# Floating point manipulation functions
+
+def copysign(x, y):
+    pass
+
+def frexp(x):
+    pass
+
+def isclose(a, b, *, rel_tol=0.00000003, abs_tol=0.0):
+    pass
+
+def isfinite(x):
+    pass
+
+def isinf(x):
+    pass
+
+def isnan(x):
+    pass
+
+def ldexp(x, i):
+    pass
+
+def nextafter(x, y):
+    pass
+
+def ulp(x):
+    pass
+
+# Power, exponential and logarithmic functions
+
+def cbrt(x):
+    pass
+
+def exp(x):
+    pass
+
+def exp2(x):
+    pass
+
+def expm1(x):
+    pass
+
+def log(x, base=E):
+    pass
+
+def log1p(x):
+    pass
+
+def log2(x):
+    pass
+
+def log10(x):
+    pass
+
+def pow(x, y):
+    pass
+
+def sqrt(x):
+    pass
+
+# Summation and product functions
+
+def dist(p, q):
+    pass
+
+def fsum(iterable):
+    pass
+
+def hypot(*coordinates):
+    pass
+
+def prod(iterable, *, start=1):
+    pass
+
+def sumprod(p, q):
+    pass
+
+# Angular conversion
+
+def degrees(x):
+    pass
+
+def radians(x):
+    pass
+
+# Trigonometric functions
+
+def acos(x):
+    pass
+
+def asin(x):
+    pass
+
+def atan(x):
+    pass
+
+def atan2(y, x):
+    pass
+
+def cos(x):
+    pass
+
+def sin(x):
+    pass
+
+def tan(x):
+    pass
+
+# Hyperbolic functions
+
+def acosh(x):
+    pass
+
+def asinh(x):
+    pass
+
+def atanh(x):
+    pass
+
+def cosh(x):
+    pass
+
+def sinh(x):
+    pass
+
+def tanh(x):
+    pass
+
+# Special functions
+
+def erf(x):
+    pass
+
+def erfc(x):
+    pass
+
+def gamma(x):
+    pass
+
+def lgamma(x):
+    pass
+
 # Constants
 
 PI = 3
